2be0343032730917e37c017fbce42501a178edb0
alonerosee <102573959+alonerosee@users.noreply.github.com>
2022-04-06 10:43:50 +0600

    js

---
+++ script.js
@@ -0,0 +1,38 @@
+
+// 1 задача
+let userNum = prompt("Введите число");
+userNum = Number(userNum);
+let result = userNum + userNum * 2;
+alert("Ваше число: " + result);
+
+
+// 2 задача
+let num = 46;
+let example = "string";
+let resultNum = num * 5;
+console.log(resultNum);
+console.log(example);
+
+
+// 3 задача
+let number = 5, f = "F", hello = "Привет", floatNum = 90.2;
+const number_67 = 67;
+alert(hello);
+
+
+// 4 задача
+let s = 0;
+for(let k = 0; k < 101; k += 4){
+	s += k;
+}
+console.log(s);
+
+// 5 задача
+let numb = prompt("Введите число: ");
+for(let i = numb; i > 0; --i){
+	if (numb % i == 0){
+		console.log(i);
+	}
+}
+
+
